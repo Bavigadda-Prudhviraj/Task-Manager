8dfb858fb428194537e8bb7624ac32a79445a0d5
Bavigadda <prudhviraj729@gmail.com>
2023-09-03 16:08:44 +0530

    Task validation and update task

--- taskmanager/taskmanager/src/main/java/com/prudhvi/taskmanager/controller/TaskController.java
+++ taskmanager/taskmanager/src/main/java/com/prudhvi/taskmanager/controller/TaskController.java
@@ -1,13 +1,14 @@
 package com.prudhvi.taskmanager.controller;
 
 import com.prudhvi.taskmanager.dto.CreateTaskDTO;
+import com.prudhvi.taskmanager.dto.ErrorResponseDTO;
+import com.prudhvi.taskmanager.dto.UpdateTaskDTO;
 import com.prudhvi.taskmanager.entity.TaskEntity;
 import com.prudhvi.taskmanager.service.TaskService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.ParseException;
 import java.util.List;
 
 @RestController
@@ -34,8 +35,26 @@ public class TaskController {
         return  ResponseEntity.ok(task);
     }
     @PostMapping("")
-    public ResponseEntity<TaskEntity> addTask(@RequestBody CreateTaskDTO body){
+    public ResponseEntity<TaskEntity> addTask(@RequestBody CreateTaskDTO body) throws ParseException {
         var task=taskService.addTask(body.getTitle(),body.getDescription(),body.getDeadline());
         return  ResponseEntity.ok(task);
+    }
+    @PatchMapping("/{id}")
+    public ResponseEntity<TaskEntity> updateTask(@PathVariable("id") Integer id, @RequestBody UpdateTaskDTO body)throws ParseException{
+        var task=taskService.updateTask(id,body.getDescription(),body.getDeadline(),body.getCompleted());
+        if(task==null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(task);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDTO> handleErrors(Exception e){
+        if(e instanceof ParseException){
+            return ResponseEntity.badRequest().body(new ErrorResponseDTO("Invalid date format"));
+        }
+        e.printStackTrace();
+        return  ResponseEntity.internalServerError().body(new ErrorResponseDTO("Internal Server Error"));
+
     }
 }
---
+++ taskmanager/taskmanager/src/main/java/com/prudhvi/taskmanager/dto/ErrorResponseDTO.java
@@ -0,0 +1,14 @@
+package com.prudhvi.taskmanager.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
+public class ErrorResponseDTO {
+    private  String error;
+}
